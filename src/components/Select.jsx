608ce104f5b5917c02c52e28e554939e6294a1b2
Lukas Bischof <lukas.bischof@renuo.ch>
2022-04-26 10:17:06 +0200

    Plasmic Setup

---
+++ src/components/Select.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { PlasmicSelect } from "./plasmic/blank_project/PlasmicSelect";
+import Option from "./Select__Option";
+import OptionGroup from "./Select__OptionGroup";
+
+function Select_(props, ref) {
+  const { plasmicProps, state } = PlasmicSelect.useBehavior(
+    {...props, onOpenChange: console.log}, 
+    ref
+  );
+  return <PlasmicSelect {...plasmicProps} />;
+}
+
+const Select = React.forwardRef(Select_);
+
+export default Object.assign(Select, {
+  Option,
+  OptionGroup,
+  __plumeType: "select"
+});
